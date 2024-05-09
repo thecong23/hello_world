61eea117a343a767dc6637516a30a3fa889fe6cd
thecong23 <congngo2003@gmail.com>
2024-05-09 14:03:23 +0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,14 @@
+*div#content {
+  background-color: #5e5e5e;
+  background-color: ;
+  height: 500px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Times New Roman", Times, serif;
+}
+ul,
+li {
+  list-style: none;
+}
